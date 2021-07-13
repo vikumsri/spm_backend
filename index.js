843f01fc8fdbd747b01a8e48bd8f3e8f4b186ffc
vikum <vchamindu@gmail.com>
2021-07-13 23:21:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongo = require("mongoose");
+const cors = require("cors");
+const port = 5000
+
+app.use(cors());
+app.use(bodyParser.json())
+
+
+mongo.connect(
+    "mongodb+srv://root:root@cluster0.pikdq.mongodb.net/icafDB?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+);
+
+mongo.connection.once("open", function () {
+    console.log("Mongo db connected");
+});
+
+app.route("/").get((req, res) => {
+    res.send("SPM Backend");
+});
+
+
+app.listen(port, () =>
+    console.log("Node server is running.."))
