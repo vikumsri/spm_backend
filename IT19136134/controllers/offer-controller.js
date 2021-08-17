600908c4e63451d3169414070739c4e8276fad91
Ashini98K <67262488+Ashini98K@users.noreply.github.com>
2021-08-18 00:01:43 +0530

    created api for offer update and delete

--- IT19136134/controllers/offer-controller.js
+++ IT19136134/controllers/offer-controller.js
@@ -5,6 +5,9 @@ const controller = require("../services/offer-service");
 module.exports = function () {
   router.post("/create", controller.createOffer);
   router.get("/", controller.getOffer);
-
+  router.put("/updateoffer/:id", controller.updateOffer);
+  router.get("/getMealForOffer/:id", controller.getMealForOffer);
+  router.get("/getCatergoryForOffer/:id", controller.getCatergoryForOffer);
+  router.get("/getOfferById/:id", controller.getOfferById);
   return router;
 };
--- IT19136134/services/offer-service.js
+++ IT19136134/services/offer-service.js
@@ -26,7 +26,94 @@ const getOffer = async (req, res) => {
     });
 };
 
+const getOfferById = async (req, res) => {
+  if (req.params && req.params.id) {
+    await Offer.findById(req.params.id)
+      .then((data) => {
+        res.status(200).send({ data: data });
+      })
+      .catch((error) => {
+        res.status(500).send({ error: error.message });
+      });
+  }
+};
+
+const updateOffer = async (req, res) => {
+  await Offer.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        offerName: req.body.offerName,
+        description: req.body.description,
+        discount: req.body.discount,
+        price: req.body.price,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        image: req.body.image,
+      },
+    },
+    { upsert: true },
+    function (err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+};
+
+const deleteOffer = async (req, res) => {
+  //check if the req body is empty
+  const id = req.params.id;
+  console.log(id);
+
+  //delete product data to database
+  await Offer.findByIdAndDelete(id)
+    .then((response) => {
+      console.log("Data sucessfully deleted from the mongo db!");
+
+      res.status(200).send(response);
+
+      console.log("Response sent!");
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+};
+
+const getMealForOffer = async (req, res) => {
+  if (req.params && req.params.id) {
+    await Offer.findById(req.params.id)
+      .populate("meals", "mealName")
+      .then((data) => {
+        res.status(200).send({ meals: data.meals });
+      })
+      .catch((error) => {
+        res.status(500).send({ error: error.message });
+      });
+  }
+};
+
+const getCatergoryForOffer = async (req, res) => {
+  if (req.params && req.params.id) {
+    await Offer.findById(req.params.id)
+      .populate("catergories", "categoryName")
+      .then((data) => {
+        res.status(200).send({ catergories: data.catergories });
+      })
+      .catch((error) => {
+        res.status(500).send({ error: error.message });
+      });
+  }
+};
+
 module.exports = {
   createOffer,
   getOffer,
+  updateOffer,
+  deleteOffer,
+  getMealForOffer,
+  getCatergoryForOffer,
+  getOfferById,
 };
